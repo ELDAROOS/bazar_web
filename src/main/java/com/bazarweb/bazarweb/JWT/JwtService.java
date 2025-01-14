85d0e4fce19d59a03e232caf6a42e147b69f7d26
Cumcordia <maksimmakarov228666@gmail.com>
2025-01-14 23:54:12 +0500

    12312

--- src/main/java/com/bazarweb/bazarweb/JWT/JwtService.java
+++ src/main/java/com/bazarweb/bazarweb/JWT/JwtService.java
@@ -18,13 +18,16 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
+
     @Value("${token.signing.key}")
     private String signingKey;
 
+    // Извлечение имени пользователя из токена
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
+    // Генерация JWT токена на основе данных пользователя
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         if (userDetails instanceof User customUserDetails) {
@@ -35,38 +38,50 @@ public class JwtService {
         return generateToken(claims, userDetails);
     }
 
+    // Проверка валидности токена
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String userName = extractUsername(token);
         return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
+    // Извлечение конкретных данных из токена
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
         final Claims claims = extractAllClaims(token);
         return claimsResolvers.apply(claims);
     }
 
+    // Генерация токена с дополнительными данными
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 100000 * 60 * 24))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
+                .setExpiration(new Date(System.currentTimeMillis() + 100000 * 60 * 24)) // Истечение срока действия через 24 часа
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
+    // Проверка на истечение срока действия токена
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
+    // Извлечение даты истечения срока действия токена
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    // Извлечение всех данных (claims) из токена
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(getSigningKey()).parseClaimsJws(token).getBody();
+        return Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .parseClaimsJws(token)
+                .getBody();
     }
 
+    // Получение ключа для подписи токена
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(signingKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
-    
 }
